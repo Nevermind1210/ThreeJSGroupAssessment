275768dc53baceeb988f9e42221149a0216ac37c
Utsjoshua <Joshua.E.Isaraela@student.uts.edu.au>
2023-04-27 11:49:43 +1000

    Got Some land ready
    Fixed the import for the code to get it set up

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ import * as dat from 'lil-gui'
 import { Color } from 'three'
 
 import { Sky } from 'three/examples/jsm/objects/Sky.js';
+import {ImprovedNoise} from "three/examples/jsm/math/ImprovedNoise.js";
 
 /**
  * Base
@@ -189,3 +190,63 @@ function initSky(){
 
 }
 // End Of Sky
+
+
+THREE.BufferGeometry.prototype.toQuads = ToQuads;
+
+const perlin = new ImprovedNoise();
+
+let step = 20;
+for(let z = -4; z <= 4; z ++){
+	for(let x = -4; x <= 4; x++){
+  	let p = createPlane(step, Math.random() * 0x7f7f7f + 0x7f7f7f);
+    setNoise(p.geometry, new THREE.Vector2(x, z), 2, 3);
+    p.geometry.rotateX(Math.PI * 0.5);
+    p.position.set(x, 0, z).multiplyScalar(step);
+    scene.add(p);
+  }
+}
+
+function createPlane( step, color){
+  let g = new THREE.PlaneGeometry(step, step, 25, 25).toQuads();
+  let m = new THREE.LineBasicMaterial({color: color});
+  let l = new THREE.LineSegments(g, m);
+  return l;
+}
+
+function setNoise(g, uvShift, multiplier, amplitude){
+	let pos = g.attributes.position;
+  let uv = g.attributes.uv;
+  let vec2 = new THREE.Vector2();
+  for(let i = 0; i < pos.count; i++){
+    vec2.fromBufferAttribute(uv, i).add(uvShift).multiplyScalar(multiplier);
+    pos.setZ(i, perlin.noise(vec2.x, vec2.y, 0) * amplitude );
+  }
+}
+
+function ToQuads() {
+	let g = this;
+  let p = g.parameters;
+  let segmentsX = (g.type == "TorusBufferGeometry" ? p.tubularSegments : p.radialSegments) || p.widthSegments || p.thetaSegments || (p.points.length - 1) || 1;
+  let segmentsY = (g.type == "TorusBufferGeometry" ? p.radialSegments : p.tubularSegments) || p.heightSegments || p.phiSegments || p.segments || 1;
+  let indices = [];
+  for (let i = 0; i < segmentsY + 1; i++) {
+    let index11 = 0;
+    let index12 = 0;
+    for (let j = 0; j < segmentsX; j++) {
+      index11 = (segmentsX + 1) * i + j;
+      index12 = index11 + 1;
+      let index21 = index11;
+      let index22 = index11 + (segmentsX + 1);
+      indices.push(index11, index12);
+      if (index22 < ((segmentsX + 1) * (segmentsY + 1) - 1)) {
+        indices.push(index21, index22);
+      }
+    }
+    if ((index12 + segmentsX + 1) <= ((segmentsX + 1) * (segmentsY + 1) - 1)) {
+      indices.push(index12, index12 + segmentsX + 1);
+    }
+  }
+  g.setIndex(indices);
+  return g;
+}
